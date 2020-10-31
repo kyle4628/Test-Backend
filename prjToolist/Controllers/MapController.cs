b4974764f29d3333ae37b7585877884e61f38162
kyle4628 <kyle4628@gmail.com>
2020-11-01 00:49:46 +0800

    Add api of get places and tags information to map controller 1. Get place information by gmap_id 2. Get tag informations of place query by gmap_id

--- prjToolist/Controllers/MapController.cs
+++ prjToolist/Controllers/MapController.cs
@@ -1,15 +1,100 @@
-﻿using System;
+﻿using prjToolist.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace prjToolist.Controllers
 {
     [RoutePrefix("map")]
     public class MapController : ApiController
     {
-        
+        FUENMLEntities db = new FUENMLEntities();
+
+        [Route("get_marks")]
+        [HttpPost]
+        [EnableCors("*", "*", "*")]
+        public HttpResponseMessage getMarkInfo()
+        {
+            var result = new
+            {
+                status = 0,
+                msg = "fail"
+            };
+            
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [Route("get_place_info")]
+        [HttpPost]
+        [EnableCors("*", "*", "*")]
+        public HttpResponseMessage getGooglePlaces(tGMapId gMapId)
+        {
+            var placeItem = db.places.FirstOrDefault(p => p.gmap_id == gMapId.gmap_id);
+            placeInfo placeInfo = new placeInfo();
+            var result = new
+            {
+                status = 0,
+                data = placeInfo,
+                msg = "fail"
+            };
+            if(placeItem != null)
+            {
+                placeInfo.name = placeItem.name;
+                placeInfo.phone = placeItem.phone;
+                placeInfo.address = placeItem.address;
+                placeInfo.type = placeItem.type;
+
+                result = new
+                {
+                    status = 1,
+                    data = placeInfo,
+                    msg = ""
+                };
+            }
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [Route("get_place_tag")]
+        [HttpPost]
+        [EnableCors("*", "*", "*")]
+        public HttpResponseMessage getPlaceTag(tGMapId gMapId)
+        {
+            var placeId = db.places.FirstOrDefault(p => p.gmap_id == gMapId.gmap_id).id;
+            var tagList = db.tagRelations.Where(t => t.place_id == placeId).Select(p=> p.tag_id).ToList();
+            List<tTag> tags = new List<tTag>();
+            
+            int[] item = tagList.Distinct().ToArray();
+
+            var result = new
+            {
+                status = 1,
+                data = tags,
+                msg = "fail"
+            };
+            foreach(int i in item)
+            {
+                tTag tagItem = new tTag();
+                var tag = db.tags.FirstOrDefault(t => t.id == i);
+                tagItem.id = tag.id;
+                tagItem.name = tag.name;
+                tagItem.type = tag.type;
+                tags.Add(tagItem);
+
+                result = new
+                {
+                    status = 0,
+                    data = tags,
+                    msg = ""
+                };
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
     }
 }
---
+++ prjToolist/Models/tMaps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjToolist.Models
+{
+    public class tStartPosition
+    {
+        public float lon { get; set; }
+        public float lat { get; set; }
+    }
+
+    public class tEndPosition
+    {
+        public float lon { get; set; }
+        public float lat { get; set; }
+    }
+
+    public class tGMapId
+    {
+        public string gmap_id { get; set; }
+    }
+
+    public class placeInfo
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string type { get; set; }
+    }
+
+    //public List<tTag> placeTags(tGMapId gMapId)
+    //{
+    //    FUENMLEntities db = new FUENMLEntities();
+    //    var placeId = db.places.FirstOrDefault(p=>p.gmap_id == gMapId.gmap_id).id;
+    //    var tagIdList = db.tagRelations.Where(t => t.place_id == placeId).ToList().Distinct();
+    //    List<tTag> tags = new List<tTag>();
+    //    for (int i= 0; i < tagIdList.Count();i++)
+    //    {
+    //        tTag tagItem = new tTag();
+    //        var tag = db.tags.FirstOrDefault(t => t.id == i);
+    //        tagItem.id = tag.id;
+    //        tagItem.name = tag.name;
+    //        tagItem.type = tag.type;
+    //        tags.Add(tagItem);
+    //    }
+    //    return tags;
+    //}
+
+}
